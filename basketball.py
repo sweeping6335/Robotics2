65b0970b8fd1e89c439bbc051039325f2f19bee0
sweeping6335 <lucas.gatlin@methow.org>
2024-12-17 14:55:40 -0800

    Create basketball.py

---
+++ basketball.py
@@ -0,0 +1,150 @@
+#region VEXcode Generated Robot Configuration
+from vex import *
+import urandom
+
+# Brain should be defined by default
+brain=Brain()
+
+# Robot configuration code
+left_drive_smart = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+right_drive_smart = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
+drivetrain = DriveTrain(left_drive_smart, right_drive_smart, 319.19, 295, 40, MM, 1)
+Wheel = Motor(Ports.PORT6, GearSetting.RATIO_18_1, False)
+Right_Spinner = Motor(Ports.PORT16, GearSetting.RATIO_18_1, False)
+Left_Spinner = Motor(Ports.PORT15, GearSetting.RATIO_18_1, False)
+controller_1 = Controller(PRIMARY)
+Back_Bumper = Bumper(brain.three_wire_port.a)
+Front_Bumper = Bumper(brain.three_wire_port.b)
+
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+
+
+# Make random actually random
+def initializeRandomSeed():
+    wait(100, MSEC)
+    random = brain.battery.voltage(MV) + brain.battery.current(CurrentUnits.AMP) * 100 + brain.timer.system_high_res()
+    urandom.seed(int(random))
+      
+# Set random seed 
+initializeRandomSeed()
+
+
+def play_vexcode_sound(sound_name):
+    # Helper to make playing sounds from the V5 in VEXcode easier and
+    # keeps the code cleaner by making it clear what is happening.
+    print("VEXPlaySound:" + sound_name)
+    wait(5, MSEC)
+
+# add a small delay to make sure we don't print in the middle of the REPL header
+wait(200, MSEC)
+# clear the console to make sure we don't have the REPL in the console
+print("\033[2J")
+
+
+
+# define variables used for controlling motors based on controller inputs
+drivetrain_l_needs_to_be_stopped_controller_1 = False
+drivetrain_r_needs_to_be_stopped_controller_1 = False
+
+# define a task that will handle monitoring inputs from controller_1
+def rc_auto_loop_function_controller_1():
+    global drivetrain_l_needs_to_be_stopped_controller_1, drivetrain_r_needs_to_be_stopped_controller_1, remote_control_code_enabled
+    # process the controller input every 20 milliseconds
+    # update the motors based on the input values
+    while True:
+        if remote_control_code_enabled:
+            
+            # calculate the drivetrain motor velocities from the controller joystick axies
+            # left = axis3 + axis1
+            # right = axis3 - axis1
+            drivetrain_left_side_speed = controller_1.axis3.position() + controller_1.axis1.position()
+            drivetrain_right_side_speed = controller_1.axis3.position() - controller_1.axis1.position()
+            
+            # check if the value is inside of the deadband range
+            if drivetrain_left_side_speed < 5 and drivetrain_left_side_speed > -5:
+                # check if the left motor has already been stopped
+                if drivetrain_l_needs_to_be_stopped_controller_1:
+                    # stop the left drive motor
+                    left_drive_smart.stop()
+                    # tell the code that the left motor has been stopped
+                    drivetrain_l_needs_to_be_stopped_controller_1 = False
+            else:
+                # reset the toggle so that the deadband code knows to stop the left motor next
+                # time the input is in the deadband range
+                drivetrain_l_needs_to_be_stopped_controller_1 = True
+            # check if the value is inside of the deadband range
+            if drivetrain_right_side_speed < 5 and drivetrain_right_side_speed > -5:
+                # check if the right motor has already been stopped
+                if drivetrain_r_needs_to_be_stopped_controller_1:
+                    # stop the right drive motor
+                    right_drive_smart.stop()
+                    # tell the code that the right motor has been stopped
+                    drivetrain_r_needs_to_be_stopped_controller_1 = False
+            else:
+                # reset the toggle so that the deadband code knows to stop the right motor next
+                # time the input is in the deadband range
+                drivetrain_r_needs_to_be_stopped_controller_1 = True
+            
+            # only tell the left drive motor to spin if the values are not in the deadband range
+            if drivetrain_l_needs_to_be_stopped_controller_1:
+                left_drive_smart.set_velocity(drivetrain_left_side_speed, PERCENT)
+                left_drive_smart.spin(FORWARD)
+            # only tell the right drive motor to spin if the values are not in the deadband range
+            if drivetrain_r_needs_to_be_stopped_controller_1:
+                right_drive_smart.set_velocity(drivetrain_right_side_speed, PERCENT)
+                right_drive_smart.spin(FORWARD)
+        # wait before repeating the process
+        wait(20, MSEC)
+
+# define variable for remote controller enable/disable
+remote_control_code_enabled = True
+
+rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
+
+#endregion VEXcode Generated Robot Configuration
+
+myVariable = 0
+dis_timer = 3
+
+def when_started1():
+    global myVariable, dis_timer
+    pass
+
+def freeze_bot():
+    brain.screen.set_fill_color(Color.RED)
+    brain.screen.draw_rectangle(0,0,479,239)
+    drivetrat_Bumper.pressing():
+    in.stop()
+    brain.screen.set_font(FontType.PROP60)
+    brain.screen.set_cursor(2.5,8.5)
+    brain.screen.print(dis_timer)
+    wait(dis_timer)
+    brain.screen.clear_screen()
+    remote_control_code_enabled = True
+    brain.screen.set_fill_color(Color.CYAN)
+    brain.screen.draw_rectangle(0,0,479,239)
+    dis_timer = dis_timer * 2
+
+def ball_pickup():
+    global myVariable
+    Wheel.spin(FORWARD)
+    Right_Spinner.spin(FORWARD)
+    Left_Spinner.spin(REVERSE)
+
+def pickup_stop():
+    global myVariable
+    Wheel.stop()
+    Right_Spinner.stop()
+    Left_Spinner.stop()
+
+# system event handlers
+Back_Bumper.pressed(freez_bot)
+Front_Bumper.pressed(freeze_bot)
+controller_1.buttonA.pressed(ball_pickup)
+controller_1.buttonB.pressed(pickup_stop)
+# add 15ms delay to make sure events are registered correctly.
+wait(15, MSEC)
+
+when_started1()
